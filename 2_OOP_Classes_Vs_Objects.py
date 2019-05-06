e692d8e00daa277382d3cc1bb0a029eb230823c7
Sravanthi Naga <49856707+sravanthilearning@users.noreply.github.com>
2019-05-06 15:07:31 +0530

    Add files via upload

---
+++ 2_OOP_Classes_Vs_Objects.py
@@ -0,0 +1,8 @@
+class Fighters:
+
+	# Fighter is the class name; sravs is the object name i.e. an instance of Fighters class
+	def __init__(self, name):
+		self.name=name
+
+sravs=Fighters("Sravanthi")
+print(sravs.name)
